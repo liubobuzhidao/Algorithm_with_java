40657853b92b8642aaa66b6bb0ca5469a91c5ab1
liubo@wifipix.com <326189438@qq.com>
2016-05-28 16:10:14 +0800

    修饰一些文件内容

--- src/SelectSort.java
+++ src/SelectSort.java
@@ -6,13 +6,17 @@ public class SelectSort {
     // 测试使用类
     public static void main(String[] args) {
         Integer[] temp = new Integer[]{10,2,5,12,25,38,3,1};
-//        selectSortDesc(temp);
+//      selectSortDesc(temp);
         selectSortAsc(temp);
         for (int i = 0; i <temp.length; i++) {
             System.out.print(temp[i] + ",");
         }
     }
 
+    /**
+     *  降序排序
+     * @param integers
+     */
     public static void selectSortDesc(Integer[] integers){
         // 外层循环
         for (int i = 0; i < integers.length - 1; i++) {
@@ -32,7 +36,11 @@ public class SelectSort {
         }
     }
 
-    // 升序排列
+    /**
+     * 升序排列
+     *
+     * @param array
+     */
     public static void selectSortAsc(Integer[] array){
         for (int i = 0; i < array.length - 1; i++) {
             int minIndex = i;
