4bae3502d3209f10bc4bf56a20d709c2b0392a4f
liubo@wifipix.com <326189438@qq.com>
2016-05-28 16:04:22 +0800

    Initial commit

---
+++ src/SelectSort.java
@@ -0,0 +1,54 @@
+/**
+ * 选择排序
+ * Created by WifiPix-liubo on 2016/5/28.
+ */
+public class SelectSort {
+    // 测试使用类
+    public static void main(String[] args) {
+        Integer[] temp = new Integer[]{10,2,5,12,25,38,3,1};
+//        selectSortDesc(temp);
+        selectSortAsc(temp);
+        for (int i = 0; i <temp.length; i++) {
+            System.out.print(temp[i] + ",");
+        }
+    }
+
+    public static void selectSortDesc(Integer[] integers){
+        // 外层循环
+        for (int i = 0; i < integers.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < integers.length; j++) {
+                if (integers[i] < integers[j] && integers[minIndex] < integers[j]){
+                    minIndex = j;
+                }
+            }
+
+            //  交换位置
+            if (minIndex != i){
+                int temp = integers[i];
+                integers[i] = integers[minIndex];
+                integers[minIndex] = temp;
+            }
+        }
+    }
+
+    // 升序排列
+    public static void selectSortAsc(Integer[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                //  找出最小的
+                if (array[i] > array[j] && array[minIndex] > array[j]){
+                    minIndex = j;
+                }
+            }
+            //  交换位置
+            if (minIndex != i){
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+
+}
---
+++ src/Test01.java
@@ -0,0 +1,8 @@
+/**
+ * Created by WifiPix-liubo on 2016/5/28.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("the first java program in the IDEA ");
+    }
+}
